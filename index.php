6611c6b3a1c1aaf3f5d91fd3cc967ac19b64afc9
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-10 17:46:12 +0100

    Bonus 1 - No solution found

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <style>
+        .invisible {
+            display: none;
+        }
+    </style>
 
     <?php
         $hotels = [
